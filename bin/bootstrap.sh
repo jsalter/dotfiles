8289209f89654d15ceb7b947a75d525c0f3755aa
James Salter <jsalter@gmail.com>
2014-06-26 11:42:37 -0700

    Bootstrap script

---
+++ bin/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+echo "requires: git tmux fish vim"
+
+function islink {
+  test -L "$HOME/$2" || ln -s "$HOME/.dotfiles/$1" "$HOME/$2"
+}
+
+function mkpath {
+  test -d "$1" || mkdir -p "$1"
+}
+
+mkpath ~/.config/fish
+mkpath ~/bin
+mkpath ~/src
+
+test -d ~/.dotfiles || git clone https://jsalter@bitbucket.org/jsalter/dotfiles.git ~/.dotfiles
+
+islink ".tmux.conf"       ".tmux.conf"
+islink ".vimrc"           ".vimrc"
+islink ".inputrc"         ".inputrc"
+islink "fish/config.fish" ".config/fish/config.fish"
+islink "fish/functions"   ".config/fish/functions"
+
+echo done
