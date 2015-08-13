7e7feca5d4262eedd8df1dec63f3cde0df62d1d7
James Salter <jsalter@gmail.com>
2015-08-13 10:04:08 -0700

    Import terminator config

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -14,13 +14,16 @@ mkdir -p ~/src
 
 test -d ~/.dotfiles || git clone https://jsalter@bitbucket.org/jsalter/dotfiles.git ~/.dotfiles
 
-ensure_link "tmux.conf"        ".tmux.conf"
-ensure_link "vim"              ".vim"
-ensure_link "vim/vimrc"        ".vimrc"
-ensure_link "inputrc"          ".inputrc"
-ensure_link "gitignore"        ".gitignore"
-ensure_link "fish/config.fish" ".config/fish/config.fish"
-ensure_link "fish/functions"   ".config/fish/functions"
+ensure_link "tmux.conf"          ".tmux.conf"
+ensure_link "vim"                ".vim"
+ensure_link "vim/vimrc"          ".vimrc"
+ensure_link "inputrc"            ".inputrc"
+ensure_link "gitignore"          ".gitignore"
+ensure_link "fish/config.fish"   ".config/fish/config.fish"
+ensure_link "fish/functions"     ".config/fish/functions"
+if [ "$OS" = "linux" -a "$(command -v terminator)" ]; then
+  ensure_link "terminator/config"  ".config/terminator/config"
+fi
 
 test -d ~/.vim/bundle/Vundle.vim || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -N -u ~/.dotfiles/vim/bundles.vim +PluginInstall! +qall
