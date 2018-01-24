328539f99809176a663d12145f51411dfe0b4826
James Salter <347279+jsalter@users.noreply.github.com>
2018-01-23 17:21:15 -0800

    Update bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -12,7 +12,7 @@ mkdir -p ~/.config/fish
 mkdir -p ~/bin
 mkdir -p ~/src
 
-test -d ~/.dotfiles || git clone https://jsalter@bitbucket.org/jsalter/dotfiles.git ~/.dotfiles
+test -d ~/.dotfiles || git clone https://github.com/jsalter/dotfiles.git ~/.dotfiles
 
 ensure_link "tmux.conf"          ".tmux.conf"
 ensure_link "vim"                ".vim"
@@ -25,13 +25,12 @@ if [ "$OS" = "linux" -a "$(command -v terminator)" ]; then
   ensure_link "terminator/config"  ".config/terminator/config"
 fi
 
-test -d ~/.vim/bundle/Vundle.vim || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -N -u ~/.dotfiles/vim/bundles.vim +PluginInstall! +qall
-cd ~/.vim/bundle/YouCompleteMe
+vim +PluginInstall +qall
+cd ~/.vim/plugged/YouCompleteMe
 if [ "$OS" = "darwin" ]; then
-  ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
+  ~/.vim/plugged/YouCompleteMe/install.sh --clang-completer
 elif [ "$OS" = "linux" ]; then
-  ~/.vim/bundle/YouCompleteMe/install.sh
+  ~/.vim/plugged/YouCompleteMe/install.sh
 fi
 test -f ~/src/z-fish/z.fish || git clone https://github.com/roryokane/z-fish ~/src/z-fish
 
